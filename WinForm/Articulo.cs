6d759f22789e2154711d204b1aa41dbf4498e695
Braian-Valor <braian.valor@alumnos.frgp.utn.edu.ar>
2021-09-07 14:31:02 -0300

    Primer commit
    Incorpore las clases Marca, Categoria y Articulo

---
+++ WinForm/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForm {
+    class Articulo {
+        public string codigoArticulo { get; set; }
+        public string nombre { get; set; }
+        public string descripccion { get; set; }
+        public Marca marca { get; set; }
+        public Categoria categoria { get; set; }
+        public string imagen { get; set; }
+        public float precio { get; set; }
+
+    }
+}
--- WinForm/Form1.Designer.cs
+++ WinForm/Form1.Designer.cs
@@ -24,10 +24,17 @@ namespace WinForm {
         /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(783, 443);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
