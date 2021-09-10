285336fa83b4b1d6fcad7ade165e04c8ebce3b23
CastilloMatiasAgustin <71848488+CastilloMatiasAgustin@users.noreply.github.com>
2021-09-10 08:29:49 -0300

    dejo mi mini aporte

--- WinForm/Form1.Designer.cs
+++ WinForm/Form1.Designer.cs
@@ -24,20 +24,111 @@ namespace WinForm {
         /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(303, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(102, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Bienvenido a Puma";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Alta Articulo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Modificar Articulo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(414, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Eliminar Articulo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(607, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 54);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Listar Articulos";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(139, 202);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(98, 54);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Busqueda de Art. Por criterios";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(513, 202);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 54);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Articulos Detallados";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 443);
+            this.ClientSize = new System.Drawing.Size(744, 345);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tp Winforms";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- WinForm/Form1.cs
+++ WinForm/Form1.cs
@@ -13,5 +13,42 @@ namespace WinForm {
         public Form1() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form Agregar_Articulo = new Form2();
+            Agregar_Articulo.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form Modificar_Articulo = new Form3();
+            Modificar_Articulo.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form Eliminar_Articulo = new Form4();
+            Eliminar_Articulo.Show();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form Listar_Articulo = new Form5();
+            Listar_Articulo.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form Busqueda_Articulo = new Form6();
+            Busqueda_Articulo.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form Articulo_detallado = new Form7();
+            Articulo_detallado.Show();
+        }
     }
 }
